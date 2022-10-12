a798ee3054d8f52c37b877e731994e5e1e9883c4
Cirillaa <polinaas2010@gmail.com>
2022-10-12 15:47:24 +0300

    added text for more info and script for closing and opening text

---
+++ buttonText.js
@@ -0,0 +1,20 @@
+let butt1 = document.querySelector('#textButton');
+let butt2 = document.querySelector('#textButton2');
+let chelsea = document.querySelector('.chelsea-more');
+let ciri = document.querySelector('.ciri-more');
+let closeButt1 = document.querySelector('#closeButton')
+let closeButt2 = document.querySelector('#closeButton2')
+
+
+butt1.addEventListener('click', function() {
+    chelsea.classList.remove('hidden')
+})
+butt2.addEventListener('click', function() {
+    ciri.classList.remove('hidden')
+});
+closeButt1.addEventListener('click', function (){
+    chelsea.classList.add('hidden');
+})
+closeButt2.addEventListener('click', function () {
+    ciri.classList.add('hidden');
+})
